ea7b87dfa0eab3472196e2d633730b4b9907075c
iara <iarassantosdev404@gmail.com>
2025-08-20 00:04:23 -0300

    adjustment with style

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,11 +5,12 @@ export default function Skills(){
   ]
   return(
     <section id="skills" className="bg-[var(--color-background)] py-16">
-      <h2 className="text-4xl text-center font-bold mb-6 py-5 text-white">
+      <h2 className="text-4xl text-center font-bold py-2 text-white">
         Minhas <span className="text-secondary">Habilidades</span>
       </h2>
+      <p className="text-gray-400 text-center text-[20px]">Tecnologias que eu domino</p>
       {/* Ou tecnologias & ferramentas */}
-      <div className="flex flex-wrap gap-4 justify-center w-[80%] mx-auto">
+      <div className="mt-8 flex flex-wrap gap-4 justify-center w-[80%] mx-auto">
         {skills.map((skill) => (
           <span
             className="text-lg border bg-black py-2 px-5 border-gray-800 rounded-[20px] text-gray-300  font-medium hover:bg-primary/10 hover:border-primary/30 transition-all duration-300 cursor-defaul"
